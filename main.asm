72fbe04b4c1a65f618dc8c0816ab226fe832e15d
PaulLefeuvre <paullefeuvre@outlook.com>
2019-09-27 14:45:16 +0800

    Added keyboard input functionality

--- main.asm
+++ main.asm
@@ -3,7 +3,7 @@ org 0x7c00
 
 jmp start
 
-strprint:
+strprint:           ; prints a string loaded into SI to the terminal
 	mov ah, 0x0e
 .loop:
 	lodsb
@@ -14,6 +14,25 @@ strprint:
 .end:
 	ret 
 
+strinput:           ; takes input from the keyboard and loads it into keyboard_buffer
+    mov di, keyboard_buffer
+.loop:
+    mov ah, 0x00
+    int 0x16
+    cmp al, 0x0d
+    je .return
+    stosb
+    mov ah, 0x0e
+    int 0x10
+    jmp .loop     
+.return:
+    mov ah, 0x0e
+    mov al, 0x0a
+    int 0x10
+    mov al, 0x0d
+    int 0x10
+    ret
+
 start:
 	xor ax, ax
 	mov ds, ax
@@ -22,15 +41,21 @@ start:
 	mov gs, ax
 	mov ss, ax
 	mov sp, 0x9c00
+.loop:
+    call strinput
+	mov si, keyboard_buffer
+    call strprint
+    mov ah, 0x0e
+    mov al, 0x0a
+    int 0x10
+    mov al, 0x0d
+    int 0x10
+    jmp .loop
 
-	mov si, string
-	call strprint
-	mov si, string2
-	call strprint 
-
-
-string: db "Hello, World!", 0x0a, 0x0d,  0x00
+string: db "Hello, World!", 0x0a, 0x0d, 0x00
 string2: db "NandOS", 0x00
+
+keyboard_buffer times 50 DB ' ', 0x00
 	
 times 510-($-$$) db 0
 dw 0xaa55
