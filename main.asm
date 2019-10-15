2fe3ff50e89ad31608fbbddb15b8de694a2a73f5
PaulLefeuvre <paullefeuvre@outlook.com>
2019-10-15 08:40:28 +0800

    Added string clearing function

--- main.asm
+++ main.asm
@@ -3,7 +3,7 @@ org 0x7c00
 
 jmp start
 
-strprint:           ; prints a string loaded into SI to the terminal
+strprint:                   ; prints a string loaded into SI to the terminal
 	mov ah, 0x0e
 .loop:
 	lodsb
@@ -14,24 +14,35 @@ strprint:           ; prints a string loaded into SI to the terminal
 .end:
 	ret 
 
-strinput:           ; takes input from the keyboard and loads it into keyboard_buffer
-    mov di, keyboard_buffer
+strinput:                   ; takes input from the keyboard and loads it into input_buffer
+    mov di, input_buffer
 .loop:
     mov ah, 0x00
     int 0x16
-    cmp al, 0x0d
+    cmp al, 0x0d            ; if the ENTER key is pressed, exit the input loop
     je .return
     stosb
     mov ah, 0x0e
     int 0x10
     jmp .loop     
 .return:
-    mov ah, 0x0e
+    mov ah, 0x0e            ; enter a newline before returning
     mov al, 0x0a
     int 0x10
     mov al, 0x0d
     int 0x10
     ret
+
+strclear:                   ; clears a string put in DI and with a size stored in BL
+    mov al, 0x00
+.loop:
+    dec bl
+    jz .end
+    stosb
+    jmp .loop
+.end:
+    ret
+    
 
 start:
 	xor ax, ax
@@ -43,19 +54,23 @@ start:
 	mov sp, 0x9c00
 .loop:
     call strinput
-	mov si, keyboard_buffer
-    call strprint
+	mov si, input_buffer
+    call strprint           ; echo the text input right before
     mov ah, 0x0e
     mov al, 0x0a
     int 0x10
     mov al, 0x0d
     int 0x10
+    mov di, input_buffer
+    mov bl, input_len
+    call strclear
     jmp .loop
 
 string: db "Hello, World!", 0x0a, 0x0d, 0x00
 string2: db "NandOS", 0x00
 
-keyboard_buffer times 50 DB ' ', 0x00
+input_buffer times 50 DB ' ', 0x00
+input_len equ $-input_buffer
 	
 times 510-($-$$) db 0
 dw 0xaa55
